8c88fe8c4341ac9cd224af26d094d13ba1b09b22
Balaji <balaji.b84@gmail.com>
2020-07-09 15:51:58 -0700

    localcommit

--- main.tf
+++ main.tf
@@ -1,15 +1,15 @@
 # Terraform state will be stored in S3
 terraform {
   backend "s3" {
-    bucket = "terraform-bucket-alex"
+    bucket = "terraform-bucket-balaji"
     key    = "terraform.tfstate"
-    region = "us-east-1"
+    region = "us-west-1"
   }
 }
 
 # Use AWS Terraform provider
 provider "aws" {
-  region = "us-east-1"
+  region = "us-west-1"
 }
 
 # Create EC2 instance
