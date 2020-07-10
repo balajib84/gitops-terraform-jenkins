f220ad338132e502302acb59b5f46ccf941e38fd
Balaji <balaji.b84@gmail.com>
2020-07-10 15:20:16 -0700

    localcommit

--- main.tf
+++ main.tf
@@ -17,7 +17,8 @@ resource "aws_instance" "default" {
   ami                    = var.ami
   count                  = var.instance_count
   key_name               = var.key_name
-  vpc_security_group_ids = [aws_security_group.default.id]
+  #vpc_security_group_ids = [aws_security_group.default.id]
+  vpc_security_group_ids = vpc-02ad1137f69efbcc7
   source_dest_check      = false
   instance_type          = var.instance_type
 
